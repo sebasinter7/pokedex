c6c6ca126a980f6f92285852060e635c3b402a3d
Sebastian Fernandez <sebas-inter@hotmail.com>
2022-06-05 22:11:52 -0500

    Correccion Slice

--- src/store/slices/user.slice.js
+++ src/store/slices/user.slice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 // Cambiamos mySlice por el nombre de nuestro slice (usersSlice, toDosSlice...)
 export const userSlice = createSlice({
   name: 'user',
-  initialState: "j",
+  initialState: "",
   reducers: {
     changeUser: ( state, actions ) => actions.payload
   }
